e1dcf981c7be734dfc22f410d3bbf7ebd8730dc8
preeti <preetisnagarkar@gmail.com>
2020-07-12 21:34:02 +0530

    Corrected url

--- Dp/Dp/settings.py
+++ Dp/Dp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#%-#vuw_ctj1kp6#qeq%3!n_)17stsmle3*m7)5x6hc*t1e5x^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ec2-13-126-17-214.ap-south-1.compute.amazonaws.com']
+ALLOWED_HOSTS = ['http://ec2-13-126-17-214.ap-south-1.compute.amazonaws.com/user']
 
 
 # Application definition
